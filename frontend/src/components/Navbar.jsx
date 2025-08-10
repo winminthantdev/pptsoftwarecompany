cf87dcb10486cce36276279c3c74ebbce1a80f7d
winminthant <winminthant.gwin@gmail.com>
2025-08-10 20:18:41 +0630

    fixbtt

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,17 +3,13 @@ import { Link } from 'react-router'
 import { useEffect } from 'react';
 
 const Navbar = () => {
-
-  const backToTop = document.querySelector(".back-to-tops");
-
   useEffect(() => {
+    const backToTop = document.querySelector(".back-to-tops");
     let lastScroll = 0;
 
     const handleScroll = () => {
       const navbar = document.querySelector(".navbar");
-
       const currentScroll = window.scrollY || document.documentElement.scrollTop;
-
 
       // For navbar
       if (navbar) {
@@ -36,48 +32,40 @@ const Navbar = () => {
       }
     };
 
+    const handleBackToTopClick = () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
     window.addEventListener("scroll", handleScroll);
-
-
+    backToTop.addEventListener("click", handleBackToTopClick);
 
     // Cleanup
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      backToTop?.removeEventListener("click", handleBackToTopClick);
     };
   }, []);
 
-  backToTop.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  });
-
-
   return (
     <>
-      {/* <!-- start navbar  --> */}
+      {/* navbar */}
       <nav className="navbar navbar-expand-md navbar-dark fixed-top">
         <div className="container">
-
           <Link to="/" className="navbar-brand">
             <img src="https://www.pngall.com/wp-content/uploads/15/Jira-Logo-Transparent.png" width="40" alt="sitelogo" />
             <span className="d-none d-md-inline ms-2">PTT Software Co.,Ltd</span>
           </Link>
-
-          <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#nav" >
+          <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#nav">
             <span className="navbar-toggler-icon"></span>
           </button>
-
           <div id="nav" className="navbar-collapse collapse justify-content-between">
-
             <ul className="navbar-nav">
               <li className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
               <li className="nav-item"><Link to="clients" className="nav-link">Clients</Link></li>
               <li className="nav-item"><Link to="aboutus" className="nav-link">About</Link></li>
               <li className="nav-item btn-group">
-                <Link href="/softwares" className="nav-link" data-bs-toggle="dropdown">Software
-                  <FontAwesomeIcon icon="fa-solid fa-caret-down" />
+                <Link to="/softwares" className="nav-link" data-bs-toggle="dropdown">
+                  Software <FontAwesomeIcon icon="fa-solid fa-caret-down" />
                 </Link>
                 <ul className="dropdown-menu">
                   <li><Link to="/softwares" className="dropdown-item">Android App</Link></li>
@@ -89,18 +77,14 @@ const Navbar = () => {
               <li className="nav-item"><Link to="pricing" className="nav-link">Special Pricing</Link></li>
               <li className="nav-item"><Link to="contact" className="nav-link">Contact Us</Link></li>
             </ul>
-
             <div>
               <Link to="#signupmodal" className="btn btn-outline-light btn-sm rounded-pill" data-bs-toggle="modal">Sign Up</Link>
             </div>
-
           </div>
-
         </div>
       </nav>
-      {/* <!-- end navbar  --> */}
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
